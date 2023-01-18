c64eb1c3a351258e6663e8ad5498c9381836a6c8
Julian <123024265+CallMeJuRa@users.noreply.github.com>
2023-01-18 22:00:43 +0100

    Add files via upload

---
+++ page.js
@@ -0,0 +1,132 @@
+const quizData = [
+    {
+        question: "Wann wurde Immanuel Kant geboren?",
+        a:"22. Februar 1825",
+        b:"12. Februar 1804",
+        c:"22. April 1724",
+        d:"25 vor Christus",
+        correct: "c",
+    },
+    {
+        question: "Frage 2",
+        a: "Antwort 1",
+        b: "Antwort 2",
+        c: "Antwort 3",
+        d: "Antwort 4",
+        correct: "b",
+    },
+    {
+        question: "Frage 3",
+        a: "Antwort 1",
+        b: "Antwort 2",
+        c: "Antwort 3",
+        d: "Antwort 4",
+        correct: "c",
+    },
+    {
+        question: "Frage 4",
+        a: "Antwort 1",
+        b: "Antwort 2",
+        c: "Antwort 3",
+        d: "Antwort 4",
+        correct: "d",
+    },
+    {
+        question: "Frage 5",
+        a: "Antwort 1",
+        b: "Antwort 2",
+        c: "Antwort 3",
+        d: "Antwort 4",
+        correct: "a",
+    },
+    {
+        question: "Frage 6",
+        a: "Antwort 1",
+        b: "Antwort 2",
+        c: "Antwort 3",
+        d: "Antwort 4",
+        correct: "b",
+    },
+    {
+        question: "Frage 7",
+        a: "Antwort 1",
+        b: "Antwort 2",
+        c: "Antwort 3",
+        d: "Antwort 4",
+        correct: "c",
+    },
+   {
+        question: "Frage 8",
+         a: "Antwort 1",
+         b: "Antwort 2",
+         c: "Antwort 3",
+         d: "Antwort 4",
+         correct: "d",
+    },
+    {
+        question: "Frage 9",
+        a: "Antwort 1",
+        b: "Antwort 2",
+        c: "Antwort 3",
+        d: "Antwort 4",
+        correct: "a",
+    },
+    {
+        question: "Frage 10",
+        a: "Antwort 1",
+        b: "Antwort 2",
+        c: "Antwort 3",
+        d: "Antwort 4",
+        correct: "b",
+                                    },
+]
+
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+let currentQuiz = 0
+let score = 0
+loadQuiz()
+function loadQuiz() {
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuiz]
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+       currentQuiz++
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>Du hast ${score} von ${quizData.length} Fragen richtig. GeGe</h2>
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
+    }
+})
